c196509f333b93fb642ab2ebaa850c1071ada0ff
Henrik Loeser <hloeser@de.ibm.com>
2019-06-24 14:12:07 +0200

    move to cf commands

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,6 @@ ibmcloud resource service-instance-delete ghstatsAppID
 # Delete DDE
 ibmcloud resource service-instance-delete ghstatsDDE
 # Delete Db2 Warehouse
-ibmcloud service delete ghstatsDB
+ibmcloud cf delete-service ghstatsDB
 # Finally remove the app
 ibmcloud cf delete github-traffic-stats
