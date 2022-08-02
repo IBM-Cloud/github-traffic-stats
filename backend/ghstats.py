a69466a8e0199ddc51a3f730769be4032713917e
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2022-08-02 15:02:23 -0400

    Update ghstats.py
    - removed print statement

--- backend/ghstats.py
+++ backend/ghstats.py
@@ -80,7 +80,6 @@ ALL_CONFIGURED=False
 # CE_SERVICES instead of Cloud Foundry VCAP_SERVICES
 if 'CE_SERVICES' in os.environ:
     vcapEnv=json.loads(os.environ['CE_SERVICES'])
-    print(vcapEnv)
 
     # Db2, either Db2 Warehouse or Db2 (lite plan)
     record=None
